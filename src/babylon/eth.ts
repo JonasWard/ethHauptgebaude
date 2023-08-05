980800e063c2656136269efc7c9447f405093ec6
JonasWard <43600893+JonasWard@users.noreply.github.com>
2023-08-05 12:19:10 +0200

    interpolate arc method

--- src/babylon/eth.ts
+++ src/babylon/eth.ts
@@ -1,3 +1,13 @@
+const interpolateArc = (o: Vector3, xDir: Vector3, yDir: Vector3): Vector3[] => {
+  const aStep = (Math.PI * 0.5) / arcPrecisionHigh;
+  const positions: Vector3[] = [];
+  for (let i = 0; i < arcPrecisionHigh + 1; i++) {
+    const a = aStep * i;
+    positions.push(o.add(xDir.scale(Math.cos(a))).add(yDir.scale(Math.sin(a))));
+  }
+
+  return positions;
+};
 const loftProfiles = (positions: Vector3[][], close: boolean = true): MeshType => {
   const mesh: MeshType = { positions: [], faces: [] };
   mesh.positions = positions.flat();
