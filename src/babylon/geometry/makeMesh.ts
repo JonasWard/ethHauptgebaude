06676b31cb6b13413888ca3d8c7cca0b70649e83
JonasWard <43600893+JonasWard@users.noreply.github.com>
2023-08-05 10:05:24 +0200

    add proper normals to mesh

--- src/babylon/geometry/makeMesh.ts
+++ src/babylon/geometry/makeMesh.ts
@@ -5,5 +5,7 @@ export const addMeshToScene = (mesh: MeshType, scene: Scene) => {
   const vertexData = new VertexData();
   vertexData.positions = mesh.positions.map((p) => [p.x, p.y, p.z]).flat();
   vertexData.indices = mesh.faces.map((f) => (f.length === 3 ? f : [f[0], f[1], f[2], f[0], f[2], f[3]])).flat();
-  vertexData.applyToMesh(new Mesh('mesh', scene), true);
+  const babylonMesh = new Mesh('mesh', scene);
+  vertexData.applyToMesh(babylonMesh, true);
+  babylonMesh.convertToFlatShadedMesh();
 };
