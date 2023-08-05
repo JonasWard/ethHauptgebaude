68c58e96acb151dece57ee99b85480a7d3a90662
JonasWard <43600893+JonasWard@users.noreply.github.com>
2023-08-05 10:05:35 +0200

    add anti-aliasing to object

--- src/Components/Renderer.tsx
+++ src/Components/Renderer.tsx
@@ -1,4 +1,4 @@
-import { ArcRotateCamera, Color4, HemisphericLight, Scene, Vector3 } from '@babylonjs/core';
+import { ArcRotateCamera, Color4, DefaultRenderingPipeline, HemisphericLight, Scene, Vector3 } from '@babylonjs/core';
 import * as React from 'react';
 import GeometrySidebar from './GeometryGeneration/GeometrySidebar';
 import SceneComponent from './Scene';
@@ -10,11 +10,12 @@ const onSceneReady = (scene: Scene, canvas: HTMLCanvasElement) => {
   const light = new HemisphericLight('ambient', new Vector3(0, 1, 0), scene);
   scene.clearColor = new Color4(1, 0.9, 0.9, 1);
   light.intensity = 1.5;
+  const camera: ArcRotateCamera = new ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 55, Vector3.Zero(), scene);
+  camera.attachControl(canvas, true);
+  const pipeline = new DefaultRenderingPipeline('default', true, scene, [camera]);
+  pipeline.samples = 1;
 
   scene.registerBeforeRender(() => {});
-
-  var camera: ArcRotateCamera = new ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 55, Vector3.Zero(), scene);
-  camera.attachControl(canvas, true);
 };
 
 const onRender = (scene: Scene) => {};
