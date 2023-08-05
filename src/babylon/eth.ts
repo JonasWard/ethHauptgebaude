565b96662bfa43b9a2437a9fb3031031f0ef875e
JonasWard <43600893+JonasWard@users.noreply.github.com>
2023-08-05 12:19:53 +0200

    construct profile method parametric

--- src/babylon/eth.ts
+++ src/babylon/eth.ts
@@ -14,6 +14,40 @@ const interpolateArc = (o: Vector3, xDir: Vector3, yDir: Vector3): Vector3[] => 
 
   return positions;
 };
+
+const mainProfile = (xDir: Vector3, zDir: Vector3): Vector3[] => {
+  const r0 = 20;
+  const r = 100;
+  const thv0 = 5;
+  const thv1 = 25;
+  const thh0 = 5;
+  const h0 = 150;
+  const h1 = 170;
+  const h2 = 140;
+
+  const outerR = r0 + r + thv0 + thv1;
+  const topH = h0 + h1 + thh0 + h2 + r;
+
+  const domeOrigin = zDir.scale(h0 + h1 + thh0 + h2);
+  const domeXDir = xDir
+    .scale(r0 + r)
+    .add(zDir.scale(h0 + h1 + thh0 + h2))
+    .subtract(domeOrigin);
+  const domeYDir = xDir.scale(r0).add(zDir.scale(topH)).subtract(domeOrigin);
+  return [
+    xDir.scale(outerR),
+    xDir.scale(outerR).add(zDir.scale(h0)),
+    xDir.scale(outerR - thv1).add(zDir.scale(h0)),
+    xDir.scale(outerR - thv1).add(zDir.scale(h0 + h1)),
+    xDir.scale(outerR).add(zDir.scale(h0 + h1)),
+    xDir.scale(outerR).add(zDir.scale(h0 + h1 + thh0)),
+    xDir.scale(outerR - thv1).add(zDir.scale(h0 + h1 + thh0)),
+    xDir.scale(outerR - thv1).add(zDir.scale(h0 + h1 + thh0 + h2)),
+    ...interpolateArc(domeOrigin, domeXDir, domeYDir),
+    zDir.scale(topH),
+  ];
+};
+
 const loftProfiles = (positions: Vector3[][], close: boolean = true): MeshType => {
   const mesh: MeshType = { positions: [], faces: [] };
   mesh.positions = positions.flat();
